ec5faef6a5b71ffbd5cf9c5554d39f97a07ad812
KARTIK KUMAR <118910009+Websterverse@users.noreply.github.com>
2024-04-20 15:31:41 +0530

    Update client.js

--- public/client.js
+++ public/client.js
@@ -13,7 +13,7 @@ textarea.addEventListener('keyup', (e) => {
     }
 })
 
-textarea.addEventListener('keydown', (e) => {
+textarea.addEventListener('touchstart', (e) => {
     if(e.keyCode === 18) {
         sendMessage(e.target.value)
     }
